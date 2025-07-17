b7078f82894d4e44ae74fa8ee532f3d0ef5a6530
Noah-Ninsiima <ninsiima30@gmail.com>
2025-07-17 12:48:38 +0300

    Update folder structure and make edits to projects

---
+++ set2Fizzb/fizz.js
@@ -0,0 +1,26 @@
+function fizzBuzz(stringOne, stringTwo){
+    if(typeof stringOne !=='string' && typeof stringTwo !== 'string'){
+        return "Both Inputs should be strings";
+    }
+    else{
+        let combinedStringLength = stringOne.length + stringTwo.length;
+
+        if(combinedStringLength % 3 === 0 && combinedStringLength % 5 === 0){
+            return "FizzBuzz";
+        }
+        else if(combinedStringLength % 3 === 0 && combinedStringLength % 5 !== 0){
+            return "Fizz";
+        }
+        else if(combinedStringLength % 5 === 0 && combinedStringLength % 3 !== 0){
+            return "Buzz";
+        }
+        else{
+            return "combined length is not divissible by either 3 or 5";
+        }
+    }
+}
+
+    fizzBuzz(10, 11);
+
+// 
+//
